2ee85a65bc0d774d4ceb9052edcf4323ef7730af
Ivo Sturm <ivo.sturm@firstconsulting.nl>
2020-11-10 11:41:35 +0100

    Moved to newer version of google API
    Moved from jsapi to loader api

--- src/GeoMap/widget/GeoMap.js
+++ src/GeoMap/widget/GeoMap.js
@@ -4,9 +4,9 @@
     ========================
 
     @file      : GeoMap.js
-    @version   : 2.0
+    @version   : 2.1
     @author    : Ivo Sturm
-    @date      : 13-2-2017
+    @date      : 10-11-2020
     @copyright : First Consulting
     @license   : Apache V2
 
@@ -15,21 +15,23 @@
     With this widget it is possible to show a GeoMap from Google on your webpage.
 	
 	v2.0: upgrade to Mendix 7
+	v2.1: - upgraded to Mendix 8
+		  - moved to new loading of Google Charts via loader library
+		  - clean up code
 	
 */
 // Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 define([
     "dojo/_base/declare",
-	"dojo/NodeList-traverse",
     "mxui/widget/_WidgetBase",
 	'dijit/_TemplatedMixin',
 	"mxui/dom",
 	"dojo/dom-style",
 	"dijit/registry",
 	"dojo/on",
-	"GeoMap/lib/jsapi",
+	"GeoMap/lib/loader",
 	'dojo/text!GeoMap/widget/template/GeoMap.html'
-], function(declare, NodeList, _WidgetBase, _TemplatedMixin, dom, domStyle, registry,on,GeoMap,widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, domStyle, registry,on,GeoMap,widgetTemplate) {
     'use strict';
 
     // Declare widget's prototype.
@@ -70,7 +72,8 @@ define([
 		googleMapsLoaded	: false,
 		schema              : null,
 		refs 				: null,
-		widgetLogNode		: "GeoChart widget:",
+		_logNode		: "GeoChart widget:",
+
 
 		// dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
         constructor: function() {
@@ -88,54 +91,92 @@ define([
 			this.domNode.style.border = this.mapBorderRadius + 'px solid' + this.mapBorderColor;
 						
 			dojo.addClass(this.domNode, 'geoChartWidget');
-			
-		
+
+
+								
 		},
 		update : function(obj, callback){
 			
 			this._contextObj = obj;
-            this._resetSubscriptions();	
+			this._resetSubscriptions();	
 
-			if (!google.visualization){
-				if (this.enableLogging){
-					console.log('visualization library not loaded yet -> loading..');
+			if (this.apiAccessKey && this.apiAccessKey !== "") {		
+				if (!google) {
+					console.warn("Google library is not loaded!");
+					return;
 				}
-                var params = null;
-				if (this.apiAccessKey !== "") {
-					params = "key=" + this.apiAccessKey  + "&sensor=false";
+
+				if (!google.visualization) {
+					if (!window._googleVisualizationLoading) {
+						window._googleVisualizationLoading = true;
+						google.charts.load('current', {
+							'packages':['geochart'],
+							// Note: you will need to get a mapsApiKey for your project.
+							// See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+							'mapsApiKey': this.apiAccessKey
+							});
+						google.charts.setOnLoadCallback(dojo.hitch(this, function() {
+							window._googleVisualizationLoading = false;
+							this.drawChart(callback);
+						}));
+					} else {
+						this._waitForGoogleLoad(callback);
+					}
 				} else {
-					params = "sensor=false";
+					this.drawChart(callback);
 				}
-				
-				google.load('visualization', '1.0', 
-				{ packages: ['corechart', 'bar', 'table'], mapsApiKey : this.apiAccessKey,  callback: dojo.hitch(this,function(){ this.drawChart(callback) })});
-
-            } else {
-				if (this.enableLogging){
-					console.log('visualization library already loaded -> reload map');
-                }
-				this.drawChart(callback);
-                 
-            }			
-
+			}  else {
+				var message = " Google Geochart needs an active API key to work!";
+				console.error(this._logNode + message);
+				alert(message);
+			}	
 		},  
+		_waitForGoogleLoad: function(callback) {
+            logger.debug(this.id + "._waitForGoogleLoad");
+            var interval = null,
+                i = 0,
+                timeout = 5000; // We'll timeout if google is not loaded
+            var intervalFunc = lang.hitch(this, function() {
+                i++;
+                if (i > timeout) {
+                    logger.warn(this.id + "._waitForGoogleLoad: it seems Google is not loaded in the other widget. Quitting");
+					this._executeCallback(callback);
+                    clearInterval(interval);
+                }
+                if (!window._googleVisualizationLoading) {
+                    this.drawChart(callback);
+                    clearInterval(interval);
+                }
+            });
+            interval = setInterval(intervalFunc, 1);
+		},
+		_executeCallback: function(cb, from) {
+            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        },
 		drawChart : function (callback) {
+
+			if (window._googleVisualizationLoading) {
+                this._waitForGoogleLoad(callback);
+            } else {
 			// Run this as soon as google charts is loaded.
 			// Create map and its container.
 
-			if (!this.geoMap) {
+				if (!this.geoMap) {
 
-				this.geoMap = new google.visualization.GeoChart(this.geoMapContainer);	
-								
-			}
+					this.geoMap = new google.visualization.GeoChart(this.geoMapContainer);	
+									
+				}
+				
+				if (this._contextObj){
+					this._getObjects(this._contextObj.getGuid,callback);
+				} else {
+					this._getObjects(null,callback);
+				}
 			
-			if (this._contextObj){
-				this._getObjects(this._contextObj.getGuid,callback);
-			} else {
-				this._getObjects(null,callback);
-			}
-			
-			this._executeCallback(callback);
+			}	
 						
 		},	
 		_getObjects : function(contextguid, callback) {
@@ -159,7 +200,7 @@ define([
 			
 			this._objectsArray = [];
 			if (this.enableLogging){
-				console.log(this.widgetLogNode + " XPathString used to retrieve Objects: " + xpathString);
+				console.log(this._lLogNode + " XPathString used to retrieve Objects: " + xpathString);
 
 			}
 			mx.data.get({
@@ -170,7 +211,7 @@ define([
 				},
 				callback    : dojo.hitch(this, this._createGeoMap, callback),
 				error       : dojo.hitch(this, function(err) {
-					console.error(this.widgetLogNode + " Unable to retrieve data: " + err);
+					console.error(this._logNode + " Unable to retrieve data: " + err);
 				})
 			});
 
@@ -204,93 +245,99 @@ define([
 				console.dir(this._objects);
 			}
 			if (this.enableLogging){ 
-				console.log(this.widgetLogNode + " Objects retrieved: ");
+				console.log(this._logNode + " Objects retrieved: ");
 				console.dir(this._objects);
 			}
-			if (this._objects.length > 0) {
-				// Create the datatable
-				
-				var dataArray = [];
-				for (var i = 0; i < this._objects.length; i++) {
-					var dataObj = [];
-					dataObj[0]= this._objects[i].country;
-					dataObj[1]= parseInt(this._objects[i].number);
+			
+			// Create the datatable
+			
+			var dataArray = [];
+			for (var i = 0; i < this._objects.length; i++) {
+				var dataObj = [];
+				dataObj[0]= this._objects[i].country;
+				dataObj[1]= parseInt(this._objects[i].number);
 
-					dataArray.push(dataObj);		
-				}
-				var headerData = ['Country', this.legendName];
-				dataArray.unshift(headerData);
-
-				var data = this._loadData(dataArray);
-				
-				google.visualization.events.addListener(this.geoMap, 'select', dojo.hitch(this, function() {
-				  var selection = this.geoMap.getSelection()[0];
-				  var label = data.getValue(selection.row, 0);
-				  var guid = 0;
-				  for (var k=0 ; k < this._objects.length ; k++){
-					 if (this._objects[k].country == label){
-						guid = this._objects[k].guid;
-					}
-				  }
-
-				  if (this.onClickMicroflow){
-					mx.data.action({
-						params: {
-							applyto: "selection",
-							actionname: this.onClickMicroflow,
-							guids: [guid]
-						},
-						callback: dojo.hitch(this, function (result) {
-							if (this.enableLogging){
-								console.log(this.widgetLogNode + " On Click Microflow " + this.onClickMicroflow + " succesfully triggeded !");
-							}
-						}),
-						error: function(error) {
-							console.log(this.widgetLogNode + error.description);
-						}
-					}, this);
-				}
-				}));	
-				
-				var options = {
-					  displayMode:this.dataType,
-					  region: this.region, // selected region
-					  colorAxis: {
-						 colors: [this.colorAxisStart,  this.colorAxisEnd]
-					  },
-					  backgroundColor: this.backGroundColor,
-					  datalessRegionColor: '#FFFFFF',
-					  defaultColor: '#F6F6F6',
-					  tooltip: {textStyle: {color: this.toolTipColor}, showColorCode: true}
-				};
-				if (this.showLegend){
-					options.legend =  {textStyle: {color: this.legendFontColor, fontSize: this.legendFontSize}};
-				} else {
-					options.legend = 'none';
-				}
-								
-				this.options = options;
-											
-				if (this.enableLogging){
-					console.log(this.widgetLogNode + " data: ");
-					console.dir(data);
-					console.log(this.widgetLogNode + " options: ");
-					console.dir(this.options);
-				}
-								
-				this.geoMap.draw(data, this.options);
-					
+				dataArray.push(dataObj);		
 			}
+			var headerData = ['Country', this.legendName];
+			dataArray.unshift(headerData);
+
+			var data = this._loadData(dataArray);
+			
+			google.visualization.events.addListener(this.geoMap, 'select', dojo.hitch(this, function() {
+				var selection = this.geoMap.getSelection()[0];
+				var label = data.getValue(selection.row, 0);
+				var guid = 0;
+				for (var k=0 ; k < this._objects.length ; k++){
+					if (this._objects[k].country == label){
+					guid = this._objects[k].guid;
+				}
+				}
+
+				if (this.onClickMicroflow){
+				mx.data.action({
+					params: {
+						applyto: "selection",
+						actionname: this.onClickMicroflow,
+						guids: [guid]
+					},
+					callback: dojo.hitch(this, function (result) {
+						if (this.enableLogging){
+							console.log(this._logNode + " On Click Microflow " + this.onClickMicroflow + " succesfully triggeded !");
+						}
+					}),
+					error: function(error) {
+						console.log(this._logNode + error.description);
+					}
+				}, this);
+			}
+			}));	
+			
+			var options = {
+					displayMode:this.dataType,
+					region: this.region, // selected region
+					colorAxis: {
+						colors: [this.colorAxisStart,  this.colorAxisEnd]
+					},
+					backgroundColor: this.backGroundColor,
+					datalessRegionColor: '#FFFFFF',
+					defaultColor: '#F6F6F6',
+					tooltip: {textStyle: {color: this.toolTipColor}, showColorCode: true}
+			};
+			if (this.showLegend){
+				options.legend =  {textStyle: {color: this.legendFontColor, fontSize: this.legendFontSize}};
+			} else {
+				options.legend = 'none';
+			}
+							
+			this.options = options;
+										
+			if (this.enableLogging){
+				console.log(this._logNode + " data: ");
+				console.dir(data);
+				console.log(this._logNode + " options: ");
+				console.dir(this.options);
+			}
+							
+			this.geoMap.draw(data, this.options);
+					
+			if (this._objects.length == 0)  {
+				var message = " no country data found, can't load map!";
+				console.warn(message);
+
+			}
+
+			this._executeCallback(callback);
 		},
 		_parseObjects : function (objs) {
 			var newObjs = [];
 			for (var i = 0; i < objs.length; i++) {
 				var newObj = {};
 				
-				newObj['country'] = this._checkRef(objs[i], 'country', this.countryAttr);
-				newObj['number'] = this._checkRef(objs[i], 'number', this.numberAttr);
+				newObj.country = this._checkRef(objs[i], 'country', this.countryAttr);
+				newObj.number = this._checkRef(objs[i], 'number', this.numberAttr);
 
-				newObj['guid'] = objs[i].getGuid();
+				newObj.guid = objs[i].getGuid();
 				
 				newObjs.push(newObj);
 			}
@@ -319,7 +366,7 @@ define([
 				entity: this.mapEntity,
 				callback: dojo.hitch(this, function(entity) {
 						if (this.enableLogging){
-							console.log(this.widgetLogNode + " Update on entity " + entity);
+							console.log(this._logNode + " Update on entity " + entity);
 						}
 						this.drawChart();
 				})
@@ -333,7 +380,7 @@ define([
 					entity: contextEntity,
 					callback: dojo.hitch(this, function(entity) {
 							if (this.enableLogging){
-								console.log(this.widgetLogNode + " Update on entity " + entity);
+								console.log(this._logNode + " Update on entity " + entity);
 							}
 							this.drawChart();
 					})
@@ -341,11 +388,6 @@ define([
 				this._handles.push(contextHandle);
 			}
             
-        },
-		_executeCallback: function (cb) {
-            if (cb && typeof cb === "function") {
-                cb();
-            }
         },
 		uninitialize : function(){
 						
